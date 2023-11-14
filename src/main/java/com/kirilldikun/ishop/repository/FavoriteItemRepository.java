02232a34e4ce86ef72d2251c16c28c8d4b932df4
krlld <kirilldikun25@gmail.com>
2023-11-14 21:29:41 +0300

    Add sorting by id for favorite items

--- src/main/java/com/kirilldikun/ishop/repository/FavoriteItemRepository.java
+++ src/main/java/com/kirilldikun/ishop/repository/FavoriteItemRepository.java
@@ -1,6 +1,7 @@
 package com.kirilldikun.ishop.repository;
 
 import com.kirilldikun.ishop.entity.FavoriteItem;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -14,5 +15,5 @@ public interface FavoriteItemRepository extends JpaRepository<FavoriteItem, Long
 
     Optional<FavoriteItem> findByUserIdAndProductId(Long userId, Long productId);
 
-    List<FavoriteItem> findAllByUserId(Long userId);
+    List<FavoriteItem> findAllByUserId(Long userId, Sort sort);
 }
--- src/main/java/com/kirilldikun/ishop/service/FavoriteItemService.java
+++ src/main/java/com/kirilldikun/ishop/service/FavoriteItemService.java
@@ -7,6 +7,7 @@ import com.kirilldikun.ishop.exception.ProductNotFoundException;
 import com.kirilldikun.ishop.exception.UserNotFoundException;
 import com.kirilldikun.ishop.repository.FavoriteItemRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -26,7 +27,7 @@ public class FavoriteItemService {
         if (!userService.existsById(userId)) {
             throw new UserNotFoundException();
         }
-        return favoriteItemRepository.findAllByUserId(userId).stream()
+        return favoriteItemRepository.findAllByUserId(userId, Sort.by(Sort.Direction.DESC, "id")).stream()
                 .map(this::mapToFavoriteItemResponse).toList();
     }
 
